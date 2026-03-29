7607a69a2cac4ae68e64fe8c4460db67ba333d8f
Turbero <dicon18@gmail.com>
2026-03-29 13:32:28 +0200

    Updating to version 1.0.1

--- FastSwim.cs
+++ FastSwim.cs
@@ -8,7 +8,7 @@ namespace FastSwim
     {
         public const string GUID = "Turbero.FastSwim";
         public const string NAME = "Fast Swim";
-        public const string VERSION = "1.0.0";
+        public const string VERSION = "1.0.1";
 
         private readonly Harmony harmony = new Harmony(GUID);
 
--- MODGEN/CHANGELOG.md
+++ MODGEN/CHANGELOG.md
@@ -1,5 +1,10 @@
 ### CHANGELOG
 
+## 1.0.1
+
+* Fixed compatibility with gamepads
+* Fixed compatibility with multiplayer worlds
+
 ## 1.0.0
 
 Initial version
